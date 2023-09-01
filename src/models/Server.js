5de065b6bc014f7c3c4797501844687ece9ee936
Carlos Daniel <csaavedrac@alumno.unsm.edu.pe>
2023-09-01 09:39:58 -0500

    server basic created and index.js

---
+++ src/models/Server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import compression from 'compression'
+
+class Server {
+  constructor() {
+    this.app = express()
+    this.port = process.env.PORT || 8080
+    this.paths = {
+      auth: '/api/auth',
+      users: '/api/users',
+    }
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.app.use(compression())
+    this.app.use(cors())
+    this.app.use(express.json())
+    this.app.use(express.static('public'))
+  }
+
+  routes() {
+    this.app.use(this.paths.auth)
+    this.app.use(this.paths.users)
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Server running on port', this.port);
+    })
+  }
+}
+
+
+export default Server
